4edc881ddf97cde23531e82b7dd72ddf9ff6faec
yueting <liuyueting426@hotmail.com>
2025-06-23 17:37:15 +0200

    resolved merge conflict in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home';
 import { AgendaComponent } from './agenda/agenda';
 import { LeaderboardComponent } from './leaderboard/leaderboard';
-=======
-import { Feedback } from './feedback/feedback';
->>>>>>> 79eeae9d45554f5684592b696bd145813256aa0e
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,15 +11,6 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () =>
       import('./auth/auth-routing-module').then(m => m.routes)
-<<<<<<< HEAD
   },
   { path: '**', redirectTo: '' }
-=======
-},
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth/login' }
- /* { path: 'feedback', component: Feedback },
-  { path: '', redirectTo: 'feedback', pathMatch: 'full' },
-  { path: '**', redirectTo: 'feedback' } */ 
->>>>>>> 79eeae9d45554f5684592b696bd145813256aa0e
 ];
