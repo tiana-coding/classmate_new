625a826533b632b776e97c5557d9dde24d3a8b9c
yueting <liuyueting426@hotmail.com>
2025-06-23 03:10:50 +0200

    update task module

--- src/app/tasks/components/admin-upload.component.ts
+++ src/app/tasks/components/admin-upload.component.ts
@@ -2,20 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../services/tasks.service.js';
 import { Task } from '../models/task.model.js';
 import { AuthService } from '../services/auth.service.js';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-upload',
-  templateUrl: './admin-upload.component.html'
+  standalone: true,
+  templateUrl: './admin-upload.component.html',
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class AdminUploadComponent implements OnInit {
   tasks: Task[] = [];
   editTask: Task|null = null;
+  taskForm!: FormGroup;
   classes = ['L1','L2','L3','L4'];
 
-  constructor(private svc: TasksService, public auth: AuthService) {}
+  constructor(private svc: TasksService, public auth: AuthService, private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.svc.getTasksByClass('L1').subscribe(data => this.tasks = data);
+    this.taskForm = this.fb.group({
+      class: ['', Validators.required],
+      title: ['', Validators.required],
+      dueDate: ['', Validators.required],
+      points: [0, [Validators.required, Validators.min(0)]],
+      documentUrl: [''],
+      externalUrl: ['']
+    });
   }
 
   save() {
--- src/app/tasks/components/task-list/task-list.component.ts
+++ src/app/tasks/components/task-list/task-list.component.ts
@@ -1,9 +1,20 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../models/task.model.js';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
-@Component({ selector: 'app-task-list', templateUrl: './task-list.component.html' })
+@Component({ 
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html' ,
+  imports: [CommonModule, TaskItemComponent]})
 export class TaskListComponent {
   @Input() tasks: Task[] = [];
   @Output() toggleComplete = new EventEmitter<Task>();
   @Output() delete = new EventEmitter<number>();
+
+  trackById(index: number, item: Task): number {
+    return item.id;
+  }
+
+
 }
